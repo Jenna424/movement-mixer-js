04da801a09e88db78495797cd6ce2322f4ca745d
github username <github email address>
2018-11-05 15:15:35 -0500

    add signup, login, logout routes to routing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-	root 'welcome#home'
-	resources :routines do
-		resources :results
-	end
+  root 'welcome#home'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  
+  resources :routines do
+  	resources :results
+  end
 end
