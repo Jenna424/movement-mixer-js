358295fe460cd11a5ec9243183b13d3a9b9e70b0
github username <github email address>
2018-11-05 14:02:40 -0500

    add 3 macros to Movement model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,2 +1,5 @@
 class Movement < ApplicationRecord
+  has_many :tips # A trainer can provide a training tip on how to execute the exercise movement
+  has_many :movement_routines
+  has_many :routines, through: :movement_routines
 end
