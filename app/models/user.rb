76e4c6d9f0b910b09d9a8be8b06442b44f319fb7
github username <github email address>
2018-11-05 13:51:20 -0500

    add has_secure_password macro to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  
   enum role: { unassigned: 0, client: 1, trainer: 2, admin: 3 }
+
+  has_many :clients, class_name: "User", foreign_key: "trainer_id"
+  belongs_to :trainer, class_name: "User", optional: true
+
+  has_many :routines
+  has_many :tips
+
+  has_secure_password
 end
