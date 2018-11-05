40c10917543ea99d75452d264009e7299218bf91
github username <github email address>
2018-11-05 14:07:50 -0500

    add belongs_to, validation, class method to Tip

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,12 @@
 class Tip < ApplicationRecord
+	belongs_to :movement
+	belongs_to :user # trainer creates tip 
+	validates :proper_form, presence: true
+	validates :breathing_technique, presence: true
+	validates :modification, presence: true
+	validates :challenge, presence: true
+
+	def self.by_trainer(trainer)
+		where(user: trainer)
+	end
 end
