1828f57e8c4ed34c60e007354967639ebf1f722c
github username <github email address>
2018-11-05 14:57:40 -0500

    add private method routine_params to RoutinesController

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -5,4 +5,25 @@ class RoutinesController < ApplicationController
 
   def edit
   end
+
+  private
+
+  	def routine_params
+      params.require(:routine).permit(
+        :title,
+        :difficulty_level,
+        :duration,
+        :user_id,
+        :movements_attributes => [
+          :name,
+          :movement_routines => [:technique, :sets, :reps]
+        ],
+        :equipment_attributes => [
+          :name,
+          :equipment_routines => [:quantity, :weight]
+        ],
+        :targets_attributes => [:focus],
+        :trainings_attributes => [:fitness_type]
+      )
+    end
 end
