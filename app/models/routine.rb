3c13eeed5c29e04521312eb689d6ffbf0871f969
github username <github email address>
2018-11-05 13:56:14 -0500

    add 8 has_many and has_many through to Routine

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,12 @@
 class Routine < ApplicationRecord
-	belongs_to :user
-	has_many :results
+  belongs_to :user
+  has_many :results
+  has_many :equipment_routines
+  has_many :equipment, through: :equipment_routines
+  has_many :movement_routines
+  has_many :movements, through: :movement_routines
+  has_many :routine_targets
+  has_many :targets, through: :routine_targets
+  has_many :routine_trainings
+  has_many :trainings, through: :routine_trainings
 end
