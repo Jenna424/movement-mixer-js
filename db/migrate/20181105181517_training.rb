4126cab73b00725e2349ae6fef067c121dab068e
github username <github email address>
2018-11-05 13:15:49 -0500

    add Target and Training models and tables

---
+++ db/migrate/20181105181517_training.rb
@@ -0,0 +1,4 @@
+class Training < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_181401) do
+ActiveRecord::Schema.define(version: 2018_11_05_181517) do
 
   create_table "equipment", force: :cascade do |t|
     t.string "name"
