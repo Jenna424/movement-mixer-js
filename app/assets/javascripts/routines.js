0fb4db96eac1e1e5c1cfb7c219729b92c3ef4c1e
github username <github email address>
2018-11-05 17:47:17 -0500

    start declaring Routine.getRoutines() function

--- app/assets/javascripts/routines.js
+++ app/assets/javascripts/routines.js
@@ -134,3 +134,10 @@ Routine.handleCreateFormSubmission = function() {
 Routine.prototype.formatPreview = function() {
   return Routine.routineTemplateFunction(this) // this refers to the JSON routine object on which we're calling the formatPreview() prototype method
 }
+
+Routine.getRoutines = function() {
+  $('ul.navbar-nav').on('click', "a.all-routines", function(e) {
+    e.preventDefault();
+    console.log('hijacked the view routines link')
+  })
+}
