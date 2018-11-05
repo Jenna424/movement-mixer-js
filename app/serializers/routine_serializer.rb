d428d8cec4406ee5da69228581c0812745ff89bc
github username <github email address>
2018-11-05 17:30:01 -0500

    Add comments in RoutineSerializer for user submittable attributes

--- app/serializers/routine_serializer.rb
+++ app/serializers/routine_serializer.rb
@@ -1,9 +1,9 @@
 class RoutineSerializer < ActiveModel::Serializer
   attributes :id, :title, :difficulty_level, :duration
   belongs_to :user
-  has_many :movement_routines # serialized because join table contains user-submittable attributes
+  has_many :movement_routines # serialized because join table contains user-submittable attributes (reps, sets, technique)
   has_many :movements
-  has_many :equipment_routines # serialized because join table contains user-submittable attributes
+  has_many :equipment_routines # serialized because join table contains user-submittable attributes (weight, quantity)
   has_many :equipment
   has_many :targets
   has_many :trainings
