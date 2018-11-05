fa0ec59279cf14ba60e278ce7ccf6baeef78fdc1
github username <github email address>
2018-11-05 13:13:29 -0500

    create Movement model and movements table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_181130) do
+ActiveRecord::Schema.define(version: 2018_11_05_181251) do
 
   create_table "equipment", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2018_11_05_181130) do
     t.text "technique"
     t.integer "sets"
     t.integer "reps"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movements", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
