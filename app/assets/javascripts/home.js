dca58245fae49cdc650c781c9f641de1ac177238
github username <github email address>
2018-11-06 17:28:23 -0500

    define handleExerciseGuideClick function in home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,21 @@
 // My home.js file will contain JS code for the navigation links
+// The navigation links will change depending on who the current user is (unassigned/client/trainer/admin)
+// In handleExerciseGuideClick(), due to event delegation, call jQuery .on() method directly on the <ul class="nav navbar-nav">,
+// a stable element that is always on the page
+// then target the link I want by passing "a.all-movements" as the second argument to .on() method
 
 $(function() {
   handleExerciseGuideClick()
 })
+
+function handleExerciseGuideClick() {
+  $('ul.nav').on('click', 'a.all-movements', function(e) {
+  	e.preventDefault();
+	console.log("HIJACKED 'READ EXERCISE GUIDE' LINK CLICK")
+	fetch(`/movements.json`)
+	  .then(response => response.json())
+	  .then(movementsArray => {
+	  	console.log(movementsArray)
+	  })
+	})
+}
