ceab65b0539b623feb51e4204bf8ecd46466f725
github username <github email address>
2018-11-06 19:47:28 -0500

    add movement_routines and routines to Movement constructor function

--- app/assets/javascripts/movements.js
+++ app/assets/javascripts/movements.js
@@ -2,4 +2,6 @@ function Movement(movement) {
 	this.id = movement.id
 	this.name = movement.name
 	this.tips = movement.tips
+	this.movement_routines = movement.movement_routines // to access user-submittable attributes (technique, sets, reps) stored in join table
+	this.routines = movement.routines
 }
