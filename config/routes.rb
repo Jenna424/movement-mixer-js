0f58c40e2e37e8468ca4b0374ef256244019fa17
github username <github email address>
2018-11-07 15:07:24 -0500

    add 3 routes for editing,updating,destroying movement w/ join table attributes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
   	resources :results
   end
   resources :movements, only: [:index, :show]
+  # Custom Routes:
+  # On the routine show page, if the current user designed the workout,
+  # that user should be able to click the edit button / delete button next to each movement that comprises the workout
+  # to edit just the attributes on the movement_routines join table (technique, sets and reps) /
+  # or to delete the join model association between their workout routine and that exercise movement
+  get '/routines/:routine_id/movements/:movement_id/edit' => 'movements#edit'
+  patch '/routines/:routine_id/movements/:movement_id' => 'movements#update'
+  delete '/routines/:routine_id/movements/:movement_id' => 'movements#destroy'
 end
