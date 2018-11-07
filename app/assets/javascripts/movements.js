ba7280759b7defcd85055a648eda36af3f477328
github username <github email address>
2018-11-06 19:53:28 -0500

    define formatMoveForIndex prototype method in movements.js

--- app/assets/javascripts/movements.js
+++ app/assets/javascripts/movements.js
@@ -7,6 +7,13 @@ function Movement(movement) {
 }
 
 Movement.compileListMovementTemplate = function() {
-	Movement.listMovementTemplateSource = $('#list-movement-template').html()
-	Movement.listMovementTemplateFunction = Handlebars.compile(Movement.listMovementTemplateSource)
+  Movement.listMovementTemplateSource = $('#list-movement-template').html()
+  Movement.listMovementTemplateFunction = Handlebars.compile(Movement.listMovementTemplateSource)
 }
+
+Movement.prototype.formatMoveForIndex = function() {
+  return Movement.listMovementTemplateFunction(this)
+}
+// In the context of formatMoveForIndex() prototype method,
+// this refers to the JSON movement object (see newMove in welcome.js file) 
+// on which formatMoveForIndex() is called
