4dd27d1a21c24b91e48ca8f80dd46acf90d91361
github username <github email address>
2018-11-06 21:05:47 -0500

    temporarily add user.unassigned? in resolve in MovementPolicy

--- app/policies/movement_policy.rb
+++ app/policies/movement_policy.rb
@@ -1,7 +1,7 @@
 class MovementPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.client? || user.trainer? || user.admin?
+      if user.client? || user.trainer? || user.admin? || user.unassigned?
         scope.all
       else
         scope.none
