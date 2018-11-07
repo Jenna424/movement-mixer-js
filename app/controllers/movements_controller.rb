29c3a917e42871e4d6fb480eb38a078f10a57735
github username <github email address>
2018-11-06 21:32:26 -0500

    define movements#show

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -3,4 +3,12 @@ class MovementsController < ApplicationController
   	@movements = policy_scope(Movement)
   	render json: @movements
   end
+
+  def show
+  	@movement = Movement.find(params[:id])
+  	respond_to do |format|
+  	  format.html
+  	  format.json {render json: @movement}
+  	end
+  end
 end
