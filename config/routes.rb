d3a4c67ff7b3cbf57026c1df80953d7b5194a55a
github username <github email address>
2018-11-06 19:19:57 -0500

    change root route to 'welcome#welcome'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
+  root 'welcome#welcome'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
