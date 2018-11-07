f865726693db7c9479023bedeee94b8162c38f96
github username <github email address>
2018-11-07 15:23:14 -0500

    stub out #edit, #update and #destroy actions in MovementsController

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -4,6 +4,15 @@ class MovementsController < ApplicationController
   	render json: @movements
   end
 
+  def edit # get '/routines/:routine_id/movements/:movement_id/edit' => 'movements#edit'
+  end
+
+  def update # PATCH "/routines/:routine_id/movements/:movement_id" => 'movements#update'
+  end
+
+  def destroy # DELETE "/routines/:routine_id/movements/:movement_id" => 'movements#destroy'
+  end
+
   def show
   	@movement = Movement.find(params[:id])
   	respond_to do |format|
