5bc3b3b545a6a469eb6902118dae83b17774d618
github username <github email address>
2018-11-08 07:27:37 -0500

    delete checkIfTemplatesExist from application.js; require templates.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,40 +14,5 @@
 //= require jquery
 //= require bootstrap
 //= require activestorage
+//= require templates
 //= require_tree .
-
-$(function() {
-  checkIfTemplatesExist()
-})
-
-function checkIfTemplatesExist() {
-  // The following variables store TRUE or FALSE (Self-Reminder: 0 is a falsy value in JS)
-  var routineTemplateExists = $('#routine-template').length
-  var userWorkoutsTemplateExists = $('#user-workouts-template').length
-  var techniqueTemplateExists = $('#technique-template').length
-  var listWorkoutTemplateExists = $('#list-workout-template').length // <script id="list-workout-template"> is found in _navbar.html.erb file
-  var listExerciseTemplateExists = $('#list-exercise-template').length // <script id="list-exercise-template"> is found in _navbar.html.erb file
-  // Conditionally compile Handlebars templates, depending on if the Handlebars template is present in the current DOM
-  if (routineTemplateExists) { // User creates a new workout routine at "/routines/new", where these HS templates are rendered
-    Routine.compileTemplates()
-    console.log("Compiled Handlebars Templates for Routine Form")
-  } else if (userWorkoutsTemplateExists) { // Currently on user show page, where link is clicked to view that user's workout routines
-    User.compileUserWorkoutsTemplate()
-    console.log("Compiled the User Workouts Handlebars Template!")
-  } else if (techniqueTemplateExists) { // User is on the routine show page
-    Routine.compileTechniqueTemplate()
-    console.log("Compiled the Technique Template!")
-  } else {
-    console.log("The current DOM page only contains the 2 Handlebars templates found in _navbar partial.")
-  }
-
-  if (listExerciseTemplateExists) {
-    Movement.compileListExerciseTemplate()
-    console.log("Compiled the List Exercise Template found in _navbar partial!")
-  }
-
-  if (listWorkoutTemplateExists) {
-    Routine.compileListWorkoutTemplate()
-    console.log("Compiled the List Workout Template found in _navbar partial!")
-  }
-}
