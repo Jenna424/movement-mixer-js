648a8069dc16541ec102456a9110fe18895dbb58
github username <github email address>
2018-11-10 17:14:30 -0500

    add private method oneself to UserPolicy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,4 +2,10 @@ class UserPolicy < ApplicationPolicy
   def new?
   	true unless user
   end
+
+  private
+
+    def oneself # the user logged in is the selfsame user (record) whose profile is being viewed
+      user == record
+    end
 end
