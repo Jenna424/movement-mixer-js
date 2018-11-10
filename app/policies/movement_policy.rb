a04af5572114e192abd3ae6810ac7adfc20b8e8e
github username <github email address>
2018-11-10 18:07:45 -0500

    define #show? in MovementPolicy class

--- app/policies/movement_policy.rb
+++ app/policies/movement_policy.rb
@@ -8,4 +8,12 @@ class MovementPolicy < ApplicationPolicy
       end
     end
   end
+
+  def show?
+  	if user.client? || user.trainer? || user.admin?
+  	  scope.all
+  	else
+  	  scope.none
+  	end
+  end
 end
