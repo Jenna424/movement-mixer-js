b41de26365090902101457ffa7591f6d5bebbeaa
github username <github email address>
2018-11-10 17:17:20 -0500

    define #show? in UserPolicy class

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,11 +1,21 @@
 class UserPolicy < ApplicationPolicy
-  def new?
+  def new
   	true unless user
+  end
+
+  def show?
+    if user.admin? # An admin can see every user show page
+      true
+    elsif user.trainer? # A trainer can see her own show page and her own clients' show pages
+      oneself || record.trainer == user
+    elsif user.client? # A client can see her own show page
+      oneself
+    end
   end
 
   private
 
-    def oneself # the user logged in is the selfsame user (record) whose profile is being viewed
-      user == record
-    end
+  	def oneself # the user logged in is the selfsame user (record) whose profile is being viewed
+  	  user == record
+  	end
 end
