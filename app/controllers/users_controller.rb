ff7d4d40b87aef5556ae5df274b54f05c9857bbb
github username <github email address>
2018-11-10 17:07:35 -0500

    add :edit, :update, :destroy to before_action :set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :login_required, only: [:new, :create]
 
   def new
