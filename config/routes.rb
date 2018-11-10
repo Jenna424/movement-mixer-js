906a3a0dde93555792694130bcda4095d9ebb98d
github username <github email address>
2018-11-10 17:28:19 -0500

    add get '/access' => 'users#access' route in config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/access' => 'users#access'
+  
   resources :users, except: [:new]
   resources :routines do
   	resources :results
