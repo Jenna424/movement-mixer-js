a3a500fa71b4171ad311dacb890acd0837c22104
github username <github email address>
2018-11-10 17:20:27 -0500

    add ? to #new?, define #edit?,#permitted_attributes,#update?,#destroy?

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,6 +1,6 @@
 class UserPolicy < ApplicationPolicy
-  def new
-  	true unless user
+  def new?
+    true unless user
   end
 
   def show?
@@ -13,9 +13,35 @@ class UserPolicy < ApplicationPolicy
     end
   end
 
+  def edit?
+    oneself
+  end
+
+  def permitted_attributes
+    if oneself && record.unassigned?
+      [:name, :email, :password, :password_confirmation, :role_requested]
+    elsif oneself
+      [:name, :email, :password, :password_confirmation]
+    elsif user.admin?
+      [:role, :trainer_id]
+    end
+  end
+
+  def update?
+    oneself || user.admin?
+  end
+
+  def destroy? # Only admins and the user who owns her account can delete her account
+    if user.unassigned? || user.client? || user.trainer?
+      oneself
+    elsif user.admin?
+      true unless oneself # An admin cannot delete her own account
+    end
+  end
+
   private
 
-  	def oneself # the user logged in is the selfsame user (record) whose profile is being viewed
-  	  user == record
-  	end
+    def oneself # the user logged in is the selfsame user (record) whose profile is being viewed
+      user == record
+    end
 end
