87da472dc06c9155b40094b036e566e06cfd4c09
github username <github email address>
2018-11-11 08:06:35 -0500

    fix indentation in tip_params

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -12,9 +12,9 @@ class TipsController < ApplicationController
 
   private
 
-  	def tip_params
-  	  params.require(:tip).permit(
-  	  	:proper_form,
+    def tip_params
+      params.require(:tip).permit(
+        :proper_form,
         :breathing_technique,
         :modification,
         :challenge,
