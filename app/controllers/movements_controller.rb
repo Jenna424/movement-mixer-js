b58c7b2a6ab5663d80576813f314bdfac97cf754
github username <github email address>
2018-11-10 20:12:03 -0500

    fix spacing

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -4,17 +4,18 @@ class MovementsController < ApplicationController
   	render json: @movements
   end
 
-  def edit # get '/routines/:routine_id/movements/:movement_id/edit' => 'movements#edit'
+  def edit # GET '/routines/:routine_id/movements/:movement_id/edit' => 'movements#edit'
   end
 
   def update # PATCH "/routines/:routine_id/movements/:movement_id" => 'movements#update'
+    
   end
 
   def destroy # DELETE "/routines/:routine_id/movements/:movement_id" => 'movements#destroy'
   end
 
   def show
-  	@movement = Movement.find(params[:id])
+    @movement = Movement.find(params[:id])
   	respond_to do |format|
   	  format.html
   	  format.json {render json: @movement}
