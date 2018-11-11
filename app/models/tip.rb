548de3dc67c7b936c6abb76cfabacd4eb5f42088
github username <github email address>
2018-11-11 07:54:08 -0500

    change TrainingGuide model back to Tip model

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,12 +1,12 @@
-class TrainingGuide < ApplicationRecord
+class Tip < ApplicationRecord
   belongs_to :movement
-  belongs_to :user # trainer creates training guides
+  belongs_to :user
   validates :proper_form, presence: true
   validates :breathing_technique, presence: true
   validates :modification, presence: true
   validates :challenge, presence: true
 
   def self.by_trainer(trainer)
-  	where(user: trainer)
+    where(user: trainer)
   end
 end
--- db/migrate/20181111124606_rename_tips_training_guides.rb
+++
@@ -1,5 +0,0 @@
-class RenameTipsTrainingGuides < ActiveRecord::Migration[5.2]
-  def change
-  	rename_table :tips, :training_guides
-  end
-end
