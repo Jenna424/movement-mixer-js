1553934cd8e298af93ad2310cc12801c105df638
github username <github email address>
2018-11-11 07:50:50 -0500

    change Tip model to TrainingGuide model; fix indentation

---
+++ app/models/training_guide.rb
@@ -0,0 +1,12 @@
+class TrainingGuide < ApplicationRecord
+  belongs_to :movement
+  belongs_to :user # trainer creates training guides
+  validates :proper_form, presence: true
+  validates :breathing_technique, presence: true
+  validates :modification, presence: true
+  validates :challenge, presence: true
+
+  def self.by_trainer(trainer)
+  	where(user: trainer)
+  end
+end
