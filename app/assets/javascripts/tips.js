2df209770281af57f7f71a3a6cd0f4e80f517e24
github username <github email address>
2018-11-11 14:02:53 -0500

    define Tip.handleFormSubmission in tips.js

--- app/assets/javascripts/tips.js
+++ app/assets/javascripts/tips.js
@@ -20,3 +20,19 @@ $(function() {
 Tip.bindClickEventHandlers = function() {
   Tip.handleFormSubmission()
 }
+
+Tip.handleFormSubmission = function() {
+  $('#new_tip').on('submit', function(e) {
+    e.preventDefault()
+    var url = $(this).attr('action') // "/movements/:movement_id/tips"
+    var formData = $(this).serialize()
+    $.post(url, formData)
+    .done(function(response) {
+      $div = $('#latest-tip') // $div variable stores jQuery object of <div id="latest-tip">
+      $div.html('') // empty out <div id="latest-tip"> in case it contains a stale tip
+      var newTip = new Tip(response) // response is JSON object representation of @tip AR instance just created in tips#create. Due to belongs_to :movement and belongs_to :user macros in TipSerializer, the response also includes data about the movement and user to which the tip belongs
+      var tipHtml = newTip.formatShow() 
+      $div.html(tipHtml)
+    })
+  })
+}
