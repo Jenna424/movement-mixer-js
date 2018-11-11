a7894b245d4922ae3ce7f4ba21777f01de59cbad
github username <github email address>
2018-11-11 08:04:05 -0500

    define #create action in TipsController

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,4 +1,14 @@
 class TipsController < ApplicationController
+  def create # POST '/movements/:movement_id/tips' => 'tips#create'
+    @movement = Movement.find(params[:movement_id]) # finding the parent movement instance
+    @tip = @movement.tips.build(tip_params)
+    authorize @tip
+    if @tip.save
+      render json: @tip
+    else
+      render "movements/show" # Exercise movement show page contains form to try creating a training tip guide belonging to that exercise movement again
+    end
+  end
 
   private
 
