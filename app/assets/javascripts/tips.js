7c55e47c0cf0d8414d4ca9ab9a64b39a8d3081ba
github username <github email address>
2018-11-11 12:30:18 -0500

    define Tip constructor function in tips.js

---
+++ app/assets/javascripts/tips.js
@@ -0,0 +1,9 @@
+function Tip(tip) {
+  this.id = tip.id
+  this.proper_form = tip.proper_form
+  this.breathing_technique = tip.breathing_technique
+  this.modification = tip.modification
+  this.challenge = tip.challenge
+  this.movement = tip.movement
+  this.user = tip.user
+}
