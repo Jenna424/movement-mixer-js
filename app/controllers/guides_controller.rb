bf91fb019cf555f3cb1ca19e0157408f19af24de
github username <github email address>
2018-11-11 20:23:14 -0500

    change TipsController to GuidesController; replace every word 'tip' with 'guide'

---
+++ app/controllers/guides_controller.rb
@@ -0,0 +1,30 @@
+class GuidesController < ApplicationController
+  def create # POST '/movements/:movement_id/guides' => 'guides#create'
+    @movement = Movement.find(params[:movement_id]) # finding the parent movement instance
+    @guide = @movement.guides.build(guide_params)
+    authorize @guide
+    if @guide.save
+      render json: @guide
+    else
+      render "movements/show" # Exercise movement show page contains form to try creating a training guide belonging to that exercise movement again
+    end
+  end
+
+  def index
+    @guides = Guide.all
+    render json: @guides
+  end
+
+  private
+
+    def guide_params
+      params.require(:guide).permit(
+        :proper_form,
+        :breathing_technique,
+        :modification,
+        :challenge,
+        :movement_id,
+        :user_id
+      )
+    end
+end
