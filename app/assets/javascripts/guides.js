c725ac6fffe8dd238b82c67979bc04ee93b12225
github username <github email address>
2018-11-11 20:04:22 -0500

    declare Guide constructor function in guides.js

---
+++ app/assets/javascripts/guides.js
@@ -0,0 +1,9 @@
+function Guide(guide) {
+  this.id = guide.id
+  this.proper_form = guide.proper_form
+  this.breathing_technique = guide.breathing_technique
+  this.modification = guide.modification
+  this.challenge = guide.challenge
+  this.movement = guide.movement
+  this.user = guide.user
+}
