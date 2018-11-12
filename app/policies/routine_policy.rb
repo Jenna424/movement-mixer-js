caa85f0dedfd42cc9efe69dca49c542fae30cd1b
github username <github email address>
2018-11-12 15:19:33 -0500

    define RoutinePolicy w/ edit?, update?, destroy?, 2 private methods

---
+++ app/policies/routine_policy.rb
@@ -0,0 +1,25 @@
+class RoutinePolicy < ApplicationPolicy
+  def edit?
+    routine_owner || routines_by_clients
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    routine_owner
+  end
+
+  private
+    # The routine (record) belongs to the logged-in user
+    def routine_owner
+      record.user == user
+    end
+    # The logged-in user is a trainer, and the routine (record) belongs to a user who is that trainer's client
+    def routines_by_clients
+      if user.trainer?
+        record.user.in?(user.clients.map {|client| client.routines}.flatten)
+      end
+    end
+end
