35811e34ff79991bbaabdcfba7de0dbd0f1fd729
github username <github email address>
2018-11-11 20:32:03 -0500

    create GuidePolicy class

---
+++ app/policies/guide_policy.rb
@@ -0,0 +1,29 @@
+class GuidePolicy < ApplicationPolicy
+  def create? # only trainers (users whose role = 2) can create training guides
+    user.trainer?
+  end
+
+  def edit? # Only the trainer who created the training guide can edit it
+    guide_owner
+  end
+
+  def permitted_attributes # user_id cannot be changed
+    if guide_owner
+      [:proper_form, :breathing_technique, :modification, :challenge]
+    end
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy? # Only the trainer who created the training guide can delete it
+    guide_owner
+  end
+
+  private
+
+    def guide_owner
+      user == record.user # The logged-in user is the same user who created the training guide (record)
+    end
+end
