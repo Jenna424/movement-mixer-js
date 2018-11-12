f865dfeeacde6ee9a972aef064c4b789a0693064
github username <github email address>
2018-11-11 19:55:36 -0500

    change has_many :tips to has_many :guides in Movement model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,5 +1,5 @@
 class Movement < ApplicationRecord
-  has_many :tips # A trainer can provide a training tip on how to execute the exercise movement
+  has_many :guides
   has_many :movement_routines
   has_many :routines, through: :movement_routines
 
