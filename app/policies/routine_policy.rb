dca3e4061f5298ce147a8aed3753ab536ee4573f
github username <github email address>
2018-11-12 16:16:20 -0500

    edit private method routines_by_clients in RoutinePolicy

--- app/policies/routine_policy.rb
+++ app/policies/routine_policy.rb
@@ -19,7 +19,7 @@ class RoutinePolicy < ApplicationPolicy
     # The logged-in user is a trainer, and the routine (record) belongs to a user who is that trainer's client
     def routines_by_clients
       if user.trainer?
-        record.user.in?(user.clients.map {|client| client.routines}.flatten)
+        record.user.in?(user.clients)
       end
     end
 end
