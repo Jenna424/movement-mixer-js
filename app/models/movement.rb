1ec6c1cc64096aae7cb6373f1fe80246c28c11fa
github username <github email address>
2018-11-13 12:29:19 -0500

    delete comment; add 2 spaces in Movement model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -2,10 +2,10 @@ class Movement < ApplicationRecord
   has_many :guides, dependent: :destroy
   has_many :movement_routines, dependent: :destroy
   has_many :routines, through: :movement_routines
-  #accepts_nested_attributes_for :movement_routines, reject_if: :all_blank
-
+  
   # Instance method #position_in_routine(routine) is called on a movement instance (self)
   # to find its numeric position in the specified routine passed in as the method's argument
+
   def position_in_routine(routine)
     movements_in_routine = Movement.joins(:routines).where(routines: { id: routine }).distinct
     movements_in_routine.find_index(self).to_i + 1
