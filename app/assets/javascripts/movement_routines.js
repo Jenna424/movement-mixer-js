1261aba34f1235e167766232adc7bdf781ae5113
github username <github email address>
2018-11-13 18:53:37 -0500

    add MovementRoutine constructor function in movement_routines.js

---
+++ app/assets/javascripts/movement_routines.js
@@ -0,0 +1,8 @@
+function MovementRoutine(movementRoutine) {
+	this.id = movementRoutine.id
+	this.movement = movementRoutine.movement
+	this.routine = movementRoutine.routine
+	this.technique = movementRoutine.technique
+	this.sets = movementRoutine.sets
+	this.reps = movementRoutine.reps
+}
