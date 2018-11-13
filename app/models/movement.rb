7484b15fa991fa495c62236b4807a0b6bb31d41c
github username <github email address>
2018-11-13 10:03:03 -0500

    fix indentation in Movement model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -7,12 +7,12 @@ class Movement < ApplicationRecord
   # Instance method #position_in_routine(routine) is called on a movement instance (self)
   # to find its numeric position in the specified routine passed in as the method's argument
   def position_in_routine(routine)
-  	movements_in_routine = Movement.joins(:routines).where(routines: { id: routine }).distinct
-  	movements_in_routine.find_index(self).to_i + 1
+    movements_in_routine = Movement.joins(:routines).where(routines: { id: routine }).distinct
+    movements_in_routine.find_index(self).to_i + 1
   end
 
   def display_technique(routine)
-  	self.movement_routines.find_by(routine: routine).technique
+    self.movement_routines.find_by(routine: routine).technique
   end
 
   def next
