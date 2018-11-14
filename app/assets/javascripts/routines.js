2b201a64044618789f3016a302256e042989f9fc
github username <github email address>
2018-11-13 20:32:49 -0500

    define Routine.displayEditExerciseForm in routines.js

--- app/assets/javascripts/routines.js
+++ app/assets/javascripts/routines.js
@@ -224,3 +224,57 @@ Routine.compileEditExerciseTemplate = function() {
   Routine.editExerciseTemplateSource = $('#edit-exercise-template').html()
   Routine.editExerciseTemplateFunction = Handlebars.compile(Routine.editExerciseTemplateSource)
 }
+
+Routine.displayEditExerciseForm = function() {
+  $(document).on('click', 'a.edit-exercise', function(e) {
+    e.preventDefault()
+    var movementId = $(this).data('exercise')
+    var $editExerciseDiv = $(`#edit-exercise-${movementId}-div`)
+    var url = $(this).attr('href') // "/routines/:routine_id/movements/:movement_id/edit"
+    $.get(url)
+    .done(function(response) {
+      $editExerciseDiv.html('')
+      let newMr = new MovementRoutine(response)
+      let mrHtml = Routine.editExerciseTemplateFunction(newMr)
+      $editExerciseDiv.html(mrHtml)
+    })
+  })
+}
+// On the routine show page, the user clicks an Edit Exercise link beside each movement that the user wants to edit in the context of that workout routine.
+// Editing an exercise just means updating the technique, sets and reps user-submittable attributes that are stored on the join table movement_routines.
+// When the user clicks the Edit Exercise link, the user sees a form to update only the technique, sets and reps of that particular exercise movement in that specific workout routine.
+// I'm going to render this form on the routine show page without a page refresh
+// Exercise movements are constantly being added/deleted from a workout routine, so due to event delegation,
+// bind the click event onto the document itself
+// Hijack the click event by calling .on() on the jQuery document object, passing in the name of the event ('click') as the 1st argument to .on()
+// The second argument passed to .on() is the <a> link element whose click event we actually want to hijack (the <a class="edit-exercise"> link)
+// Prevent the default action, which would be to redirect to "/routines/:routine_id/movements/:movement_id/edit"
+// The Edit Exercise link on the routine show page has a data-exercise property that stores the id of the movement instance I want to edit
+// Set movementId variable = the id of the movement I want to revise
+// Set JavaScript variable $editExerciseDiv = the jQuery object of the <div id="edit-exercise-ID OF MOVEMENT TO EDIT GOES HERE-div">,
+// which is where I will place the form to edit the technique, sets and reps join table attributes 
+// Reminder: this refers to the <a class="edit-exercise"> link that was clicked
+// Set variable url = href attribute value of this <a> link tag, which is the string URL "/routines/ROUTINE ID HERE/movements/MOVEMENT ID HERE/edit"
+// Use jQuery .get() method to send an AJAX GET request to "/routines/:routine_id/movements/:movement_id/edit".
+// The route '/routines/:routine_id/movements/:movement_id/edit' maps to the #edit_movement_routine action in RoutinesController 
+// (I defined a custom route in config/routes.rb)
+// $.get(url) returns jqXHR object, on which I call .done(), passing in the callback function
+// In routines#edit_movement_routine, I find the instance of MovementRoutine join model that belongs to the specific movement I want to edit in the specific routine
+// I render the JSON representation of this instance of MovementRoutine join model
+// The response passed to the callback function is the JSON object representation of the instance of MovementRoutine join model whose technique, sets and reps attributes I'm going to edit
+// Due to belongs_to :movement and belongs_to :routine macros in MovementRoutineSerializer,
+// the response also includes data about the movement and routine instances to which the MovementRoutine instance belongs
+// Clear out the <div id="edit-exercise-ID OF MOVEMENT TO BE EDITED HERE-div">, just in case it contains a stale edit form
+// Set newMr variable = a new MovementRoutine object, created with the MovementRoutine constructor function (found in movement_routines.js file)
+// and with the JSON response of the the MovementRoutine instance passed in as the argument, so that all key/value attribute data pairs can be set in the new object
+// Reminder: in my application layout file, I render the partial app/views/shared/_hs_templates.html.erb
+// In this partial, I have a <script id="edit-exercise-template", inside of which I have the Handlebars template for generating the form to edit the technique, sets and reps attributes
+// In templates.js file, I conditionally compile the template by calling Routine.compileEditExerciseTemplate() if the template exists on the page
+// In Routine.compileEditExerciseTemplate, I got the HTML string source of the <script id="edit-exercise-template">, 
+// and then I passed this string HTML into Handlebars.compile()
+// to compile the string HTML along w/ any Handlebars {{}} delimiters into a function stored as Routine.editExerciseTemplateFunction
+// I can invoke this function, passing in an object whose key names correspond to the variables between the Handlebars delimiters in the template
+// Set the variable mrHtml = Routine.editExerciseTemplateFunction(newMr), where newMr is the JS object I created with the JSON response I got back, which represents the instance of the MovementRoutine join model
+// mrHtml is the string HTML edit form with all the values filled in
+// Set this as the HTML content inside the <div id="edit-exercise-MOVEMENT ID HERE-div">
+
