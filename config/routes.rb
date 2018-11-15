c3e5aab8deea587e9a731bed6bed7d6b1278e2e8
github username <github email address>
2018-11-15 15:01:40 -0500

    add route get '/mrs/:id' => 'routines#show_technique'

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
   # On the routine show page, if the current user designed the workout routine,
   # that user should be able to click the Edit Exercise Link that is found next to each exercise movement in the workout
   # to edit the user-submittable attributes on the movement_routines join table (technique, sets and reps)
+  get '/mrs/:id' => 'routines#show_technique'
   get '/routines/:routine_id/movements/:movement_id/edit' => 'routines#edit_movement_routine'
   patch '/routines/:routine_id/movements/:movement_id' => 'routines#update_movement_routine'
-
   delete '/routines/:routine_id/movements/:movement_id' => 'movements#destroy'
 end
