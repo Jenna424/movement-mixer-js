3b72259568643cad8150ba1f7e547be1e7d2cacd
github username <github email address>
2018-11-17 12:47:31 -0500

    define MovementRoutine.destroyListener

--- app/assets/javascripts/movement_routines.js
+++ app/assets/javascripts/movement_routines.js
@@ -15,6 +15,27 @@ MovementRoutine.bindEventListeners = function() {
   MovementRoutine.destroyListener()
 }
 
+MovementRoutine.destroyListener = function() {
+  $('div.panel-default').on('click', 'form.button_to', function(e) {
+    e.preventDefault()
+    if (confirm('Are you sure you want to remove this exercise?')) {
+      $.ajax({
+        url: $(this).attr('action'), // "/routines/:routine_id/movements/:movement_id" (maps to routines#destroy)
+        method: 'delete',
+        dataType: 'json',
+        data: $(this).serialize()
+      })
+      .done(function(response) {
+        var newMr = new MovementRoutine(response)
+        var mrId = newMr.id
+        $(`div#mr-${mrId}-data`).remove()
+      })
+    } else {
+      console.log("User did not confirm exercise deletion.")
+    }
+  })
+}
+
 MovementRoutine.compileTechniqueTemplate = function() {
   MovementRoutine.techniqueTemplateSource = $('#technique-template').html()
   MovementRoutine.techniqueTemplateFunction = Handlebars.compile(MovementRoutine.techniqueTemplateSource)
