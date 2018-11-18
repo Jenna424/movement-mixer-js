83d805531d67e65ccee4c3fec8908929b6496ced
github username <github email address>
2018-11-18 17:16:51 -0500

    define er_instance(equipment, routine) helper method

--- app/helpers/routines_helper.rb
+++ app/helpers/routines_helper.rb
@@ -2,4 +2,8 @@ module RoutinesHelper
   def mr_instance(movement, routine)
   	MovementRoutine.find_by(movement: movement, routine: routine)
   end
+
+  def er_instance(equipment, routine)
+  	EquipmentRoutine.find_by(equipment: equipment, routine: routine)
+  end
 end
