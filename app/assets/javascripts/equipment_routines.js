1bd013cc150cf5728f82de525bd39eb0f23c8757
github username <github email address>
2018-11-18 18:52:04 -0500

    define EquipmentRoutine constructor function

---
+++ app/assets/javascripts/equipment_routines.js
@@ -0,0 +1,7 @@
+function EquipmentRoutine(equipmentRoutine) {
+  this.id = equipmentRoutine.id
+  this.equipment = equipmentRoutine.equipment
+  this.routine = equipmentRoutine.routine
+  this.quantity = equipmentRoutine.quantity
+  this.weight = equipmentRoutine.weight
+}
