ff9c1928abe41d865251c5b99207526a04e3e93d
github username <github email address>
2018-11-18 20:28:16 -0500

    define EquipmentRoutine.editListener in equipment_routines.js

--- app/assets/javascripts/equipment_routines.js
+++ app/assets/javascripts/equipment_routines.js
@@ -14,3 +14,20 @@ EquipmentRoutine.compileEditEquipmentRoutineTemplate = function() {
   EquipmentRoutine.editEquipmentRoutineTemplateSource = $('#edit-equipment-routine-template').html()
   EquipmentRoutine.editEquipmentRoutineTemplateFunction = Handlebars.compile(EquipmentRoutine.editEquipmentRoutineTemplateSource)
 }
+
+EquipmentRoutine.editListener = function() {
+  $('ul.required-equipment').on('click', 'a.edit-equipment', function(e) {
+  	e.preventDefault()
+  	var $editEquipmentLink = $(this)
+  	var erId = $(this).data('er-id') // stores the id of the instance of EquipmentRoutine join model being edited
+  	var $editDiv = $(`div#edit-er-${erId}-div`)
+  	$.get(`/ers/${erId}/edit`)
+  	.done(function(response) {
+  	  var newEr = new EquipmentRoutine(response)
+  	  var editFormHtml = EquipmentRoutine.editEquipmentRoutineTemplateFunction(newEr)
+  	  $editDiv.html(editFormHtml)
+  	  $editDiv.addClass('well well-md')
+  	  $editEquipmentLink.hide()
+  	})
+  })
+}
