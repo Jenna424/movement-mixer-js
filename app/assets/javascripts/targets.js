b497b59c9866f48dfddfb4fb915c0c6355393921
github username <github email address>
2018-11-20 17:07:21 -0500

    define Target.create function in targets.js

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -15,3 +15,8 @@ Target.createListener = function() {
   	.done(Target.create)
   })
 }
+// json parameter below = JSON object representation of newly created AR target instance = response from AJAX POST request sent with $.post() method in Target.createListener()
+Target.create = function(json) {
+  var newTargetArea = new Target(json)
+  newTargetArea.formatAndAppendLi()
+}
