faa3cfb7191962c63bb29159ecb14a13deedc667
github username <github email address>
2018-11-20 18:25:11 -0500

    define #index action in TargetsController

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -3,6 +3,19 @@ class TargetsController < ApplicationController
     @target_area = Target.new # instance for form_for to wrap around
   end
 
+  def index
+    @target_area = Target.new # instance for form_for to wrap around
+    @target_areas = Target.all
+    render json: @target_areas
+  end
+
+  def create
+  	@target_area = Target.new(target_params)
+  	if @target_area.save
+  	  render json: @target_area
+  	end
+  end
+
   private
 
     def target_params
