f9460a082cb84b780c405e88c85493678cf09b8e
github username <github email address>
2018-11-20 10:53:40 -0500

    whitelist :updated_at attribute in MovementRoutineSerializer

--- app/serializers/movement_routine_serializer.rb
+++ app/serializers/movement_routine_serializer.rb
@@ -1,5 +1,5 @@
 class MovementRoutineSerializer < ActiveModel::Serializer
-  attributes :id, :technique, :sets, :reps, :movement_id, :routine_id
+  attributes :id, :technique, :sets, :reps, :movement_id, :routine_id, :updated_at
   belongs_to :movement
   belongs_to :routine
 end
