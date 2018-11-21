d839ebc3b5ea76215b1fef1966d5db9f11d69cb7
github username <github email address>
2018-11-21 10:04:59 -0500

    authorize @training_type in #new; define trainings#index

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,8 +1,14 @@
 class TrainingsController < ApplicationController
   def new
-    @training_type = Training.new
+    @training_type = Training.new # instance for form_for to wrap around
+    authorize @training_type
   end
 
+  def index
+  	@training_types = policy_scope(Training)
+  	render json: @training_types
+  end
+  
   private
 
     def training_params
