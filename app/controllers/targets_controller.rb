8040c8db6719a6ef6eb0a553099fe28f3d5f1c53
github username <github email address>
2018-11-20 21:21:35 -0500

    replace instance variables w/ local variables in #destroy

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -16,9 +16,9 @@ class TargetsController < ApplicationController
   end
 
   def destroy
-    @target_area = Target.find(params[:id])
-    @target_area.destroy
-    render json: @target_area
+    target_area = Target.find(params[:id])
+    target_area.destroy
+    render json: target_area
   end
 
   private
