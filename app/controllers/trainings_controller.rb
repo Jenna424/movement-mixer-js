1fae3b063c991dc97e6b84a544541d28e4a94f51
github username <github email address>
2018-11-21 12:30:29 -0500

    add if/else logic in trainings#create to handle failure

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -4,6 +4,15 @@ class TrainingsController < ApplicationController
     authorize @training_type
   end
 
+  def create
+    training_type = Training.new(training_params)
+    if training_type.save
+      render json: training_type, status: :created # status: 201
+    else
+      render json: { errors: training_type.errors.full_messages }, status: :unprocessable_entity # status: 422
+    end
+  end
+
   def index
     @training_types = policy_scope(Training)
     render json: @training_types
