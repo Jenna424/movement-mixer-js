f6bd94627d326846485010d4595be7f2083d2b59
github username <github email address>
2018-11-20 20:09:46 -0500

    add validation for presence of focus in Target model

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,4 +2,5 @@ class Target < ApplicationRecord
   has_many :routine_targets, dependent: :destroy
   has_many :routines, through: :routine_targets
   validates :focus, uniqueness: true
+  validates :focus, presence: true
 end
