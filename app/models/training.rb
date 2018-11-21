96d32b18f3e83975b84bec73c03dc94020768264
github username <github email address>
2018-11-21 10:33:17 -0500

    add fitness_type presence,uniqueness validation in Training

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,5 @@
 class Training < ApplicationRecord
   has_many :routine_trainings, dependent: :destroy
   has_many :routines, through: :routine_trainings
+  validates :fitness_type, presence: true, uniqueness: true
 end
