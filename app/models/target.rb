7bf85b9e49d957ae8d2cad9eb3a4a61c067b7921
github username <github email address>
2018-11-21 10:32:23 -0500

    move focus presence and uniqueness validation onto 1 line

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,6 +1,5 @@
 class Target < ApplicationRecord
   has_many :routine_targets, dependent: :destroy
   has_many :routines, through: :routine_targets
-  validates :focus, uniqueness: true
-  validates :focus, presence: true
+  validates :focus, presence: true, uniqueness: true
 end
