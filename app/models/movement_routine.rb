038b79a7b93bc0b29a4be0801dde73c82e25d646
github username <github email address>
2018-11-23 20:08:37 -0500

    add presence,numericality validations for technique,sets,reps

--- app/models/movement_routine.rb
+++ app/models/movement_routine.rb
@@ -1,4 +1,7 @@
 class MovementRoutine < ApplicationRecord
   belongs_to :movement
   belongs_to :routine
+  validates :technique, presence: true
+  validates :sets, presence: true, numericality: { greater_than: 0 }
+  validates :reps, presence: true, numericality: { greater_than: 0 }
 end
