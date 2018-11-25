519fdf701c2679c64fc0c1c74927e6f03e03de1c
github username <github email address>
2018-11-25 13:49:42 -0500

    add presence/numericality validations for quantity/weight

--- app/models/equipment_routine.rb
+++ app/models/equipment_routine.rb
@@ -1,4 +1,6 @@
 class EquipmentRoutine < ApplicationRecord
   belongs_to :equipment
   belongs_to :routine
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :weight, numericality: { greater_than: 0 }
 end
