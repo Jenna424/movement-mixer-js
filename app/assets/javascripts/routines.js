4b776f513ca3b0cfda6cf50a478c20dc1a751051
github username <github email address>
2018-11-25 16:26:33 -0500

    change 'edit' to 'design' in Routine.revealErrors alert

--- app/assets/javascripts/routines.js
+++ app/assets/javascripts/routines.js
@@ -95,7 +95,7 @@ Routine.prototype.formatAndAppendPreview = function() {
 Routine.revealErrors = function(jqXhrObject) {
   var errorsArray = jqXhrObject.responseJSON.errors
   var errorsString = errorsArray.join('\n') // join array elements (string error messages) with a line break
-  alert(`Your attempt to edit this workout routine was unsuccessful:\n\n${errorsString}`)
+  alert(`Your attempt to design this workout routine was unsuccessful:\n\n${errorsString}`)
 }
 // Explanation of Routine.addAssociationToExistingWorkout function below (which replaced Routine.addExerciseListener AND Routine.addEquipmentListener functions)
 // On the Routine Show Page, the user can add an exercise movement to a workout routine by submitting form.add-exercise-form
