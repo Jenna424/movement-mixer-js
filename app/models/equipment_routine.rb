d851c1299a2403f54c6e5fb76e067a18b73fda87
github username <github email address>
2018-11-25 16:25:22 -0500

    remove numericality validation for weight in EquipmentRoutine

--- app/models/equipment_routine.rb
+++ app/models/equipment_routine.rb
@@ -2,5 +2,4 @@ class EquipmentRoutine < ApplicationRecord
   belongs_to :equipment
   belongs_to :routine
   validates :quantity, presence: true, numericality: { greater_than: 0 }
-  validates :weight, numericality: { greater_than: 0 }
 end
