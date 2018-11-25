3afd2067f0664ad14cf5078b0a1cfde54d82d943
github username <github email address>
2018-11-25 13:46:56 -0500

    add name presence,uniqueness validations to Equipment model

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,5 @@
 class Equipment < ApplicationRecord
   has_many :equipment_routines, dependent: :destroy
   has_many :routines, through: :equipment_routines
+  validates :name, presence: true, uniqueness: true
 end
