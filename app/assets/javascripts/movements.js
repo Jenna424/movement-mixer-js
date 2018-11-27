11507b71ec5c9630a0208f608226e5f21f3b1847
github username <github email address>
2018-11-27 12:01:34 -0500

    delete Movement.backToWorkoutListener

--- app/assets/javascripts/movements.js
+++ app/assets/javascripts/movements.js
@@ -13,7 +13,6 @@ $(function() {
 Movement.bindClickEventHandlers = function() {
 	Movement.indexListener()
   Movement.showListener()
-  Movement.backToWorkoutListener()
   Movement.nextExerciseListener()
   Movement.previousExerciseListener()
 }
@@ -57,12 +56,6 @@ Movement.prototype.formatMoveForIndex = function() {
 Movement.compileListExerciseTemplate = function() {
   Movement.listExerciseTemplateSource = $('#list-exercise-template').html()
   Movement.listExerciseTemplateFunction = Handlebars.compile(Movement.listExerciseTemplateSource)
-}
-
-Movement.backToWorkoutListener = function() {
-  $('button.back').on('click', function() {
-    history.back()
-  })
 }
 // Explanation of Movement.showListener
 // On the routine show page, the user can click a link to view a particular exercise movement included in that workout routine,
