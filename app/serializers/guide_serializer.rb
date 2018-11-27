06556db7da789adea5726787ed83ae4b250d704f
github username <github email address>
2018-11-27 14:46:46 -0500

    move belongs_to :movement below belongs_to :user

--- app/serializers/guide_serializer.rb
+++ app/serializers/guide_serializer.rb
@@ -1,5 +1,5 @@
 class GuideSerializer < ActiveModel::Serializer
   attributes :id, :proper_form, :breathing_technique, :modification, :challenge
-  belongs_to :movement
   belongs_to :user
+  belongs_to :movement
 end
