6f8ee63d5b1c70f4c9fffd4324003e808d937c22
github username <github email address>
2018-11-29 18:05:00 -0500

    edit comments in users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -22,13 +22,13 @@ $(function() {
 // Prevent the default behavior, which would normally redirect us to the user show page "/users/:id"
 // Store the id of the user whose workout routines we want to view in the variable id
 // Send an AJAX GET request using $.get() method to "/users/:id.json", interpolating the value of the id variable into this string URL path
-// The response to the AJAX GET request = JSON object representation of the AR user instance whose show page we're currently on (and whose workout routines we want to see)
+// userObject parameter = response we get back from AJAX GET request = JSON object representation of the AR user instance whose show page we're currently on (and whose workout routines we want to see)
 // Due to the has_many :routines macro in UserSerializer class, this JSON response also contains data about that user's workout routines
-// (namely, the user JSON object has a "routines" key that points to an array of routine objects)
+// (namely, the JSON user object has a "routines" key that points to an array of routine objects)
 // In #show action of UsersController, I specify render json: @user, include: ['routines.equipment', 'routines.targets', 'routines.trainings']
 // Therefore, not only does the JSON response representation of @user instance include data about the collection of routines that belong to that user,
-// but the JSON response ALSO includes nested data about the associated pieces of equipment, target areas and training types 
-// for those routines. (In the RoutineSerializer class, I include the 3 macros: has_many :equipment, has_many :targets and has_many :trainings)
+// but the JSON response ALSO includes nested data about the associated pieces of equipment, target areas and training types for those routines. 
+// (In the RoutineSerializer class, I include the 3 macros: has_many :equipment, has_many :targets and has_many :trainings)
 User.loadClientWorkoutsListener = function() {
   $('div#user-stats').on('click', 'a.client-workouts', function(e) {
     e.preventDefault()
