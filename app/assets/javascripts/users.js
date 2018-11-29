8342e063824cfec76368d75580c234358774d40d
github username <github email address>
2018-11-29 13:40:46 -0500

    hijack the click event of the link to view client's workouts

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,6 +9,13 @@ function User(user) {
 $(function() {
   User.loadClientWorkouts();
 })
-
+// <div id="user-stats"> is always found on the user show page.
+// However, the content inside this <div> will change depending on the role of the user whose profile page we're viewing.
+// If the user is a client, div#user-stats contains a link to view that client's workout routines.
+// If the user is a trainer, div#user-stats contains a link to view that trainer's training guides.
 User.loadClientWorkouts = function() {
+  $('div#user-stats').on('click', 'a.client-workouts', function(e) {
+  	e.preventDefault()
+  	console.log('Clicked the link to view workout routines by this client!')
+  })
 }
