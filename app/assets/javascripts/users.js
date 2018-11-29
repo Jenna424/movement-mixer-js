fbe8f2301ad7f6173a76f2efa7ec184a8b920b04
github username <github email address>
2018-11-29 13:12:34 -0500

    edit User constructor function: change tips to guides,fix indentation

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,9 @@
 function User(user) {
-	this.name = user.name
-	this.email = user.email
-	this.role = user.role
-	this.routines = user.routines
-	this.tips = user.tips
+  this.name = user.name
+  this.email = user.email
+  this.role = user.role
+  this.routines = user.routines
+  this.guides = user.guides
 }
 
 $(function() {
