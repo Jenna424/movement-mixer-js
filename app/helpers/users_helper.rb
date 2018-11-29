dab747586707704b2dfed9d95b79226e75649d55
github username <github email address>
2018-11-28 20:44:53 -0500

    define display_fitness_match(user) helper in UsersHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+  def display_fitness_match(user)
+  	if user.client?
+  	  if user.trainer.nil?
+  	  	"#{user.name} does not have a personal fitness trainer."
+  	  else
+  	  	"#{user.name} trains with Personal Trainer #{user.trainer.name}."
+  	  end
+  	elsif user.trainer?
+  	  if !user.clients.empty?
+  	  	"#{user.name} currently trains #{user.clients.count} fitness #{'client'.pluralize(user.clients.count)}."
+  	  else
+  	  	"#{user.name} is not assigned to any fitness clients."
+  	  end
+  	elsif user.admin?
+  	  "#{user.name} manages all fitness clients and personal trainers."
+  	else
+      "#{user.name} must be assigned a role in order to connect with the Movement Mixer community."
+    end
+  end
 end
