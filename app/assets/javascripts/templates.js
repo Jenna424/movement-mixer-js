05b7e1a908803c867329a5d53ec8a0cce6044ce5
github username <github email address>
2018-11-29 12:54:57 -0500

    delete var userWorkoutsTemplateExists,else if/else logic

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -5,7 +5,6 @@ $(function() {
 function checkIfTemplatesExist() {
   // The following variables store TRUE or FALSE (Self-Reminder: 0 is a falsy value in JS)
   var routineTemplateExists = $('#routine-template').length
-  var userWorkoutsTemplateExists = $('#user-workouts-template').length
   var techniqueTemplateExists = $('#technique-template').length
   var listWorkoutTemplateExists = $('#list-workout-template').length
   var listExerciseTemplateExists = $('#list-exercise-template').length
@@ -22,11 +21,6 @@ function checkIfTemplatesExist() {
   if (routineTemplateExists) { // User creates a new workout routine at "/routines/new", where these HS templates are rendered
     Routine.compileCreateTemplates()
     console.log("Compiled Handlebars Templates for Routine Form")
-  } else if (userWorkoutsTemplateExists) { // Currently on user show page, where link is clicked to view that user's workout routines
-    User.compileUserWorkoutsTemplate()
-    console.log("Compiled the User Workouts Handlebars Template!")
-  } else {
-    console.log("The current DOM page only contains the 2 Handlebars templates found in _navbar partial.")
   }
 
   if (listExerciseTemplateExists) {
