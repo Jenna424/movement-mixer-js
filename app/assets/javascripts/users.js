82853f7ee580f65b6bc2f01529619a725735fd57
github username <github email address>
2018-11-29 13:16:06 -0500

    delete code inside User.loadClientWorkouts function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,16 +11,4 @@ $(function() {
 })
 
 User.loadClientWorkouts = function() {
-  $('.user-workouts').on('click', function(e) {
-  	e.preventDefault();
-	var $div = $('#user-routines');
-	$div.html('') // empty out the <div id="user-routines"> in case it contains any stale routines
-	var id = $(this).attr('data-id');
-	$.get(`/users/${id}.json`)
-	.done(function(response) {
-	  response.routines.forEach(function(routineObject) {
-	  	$div.append(User.userWorkoutsTemplateFunction(routineObject))
-	  })
-	})
-  })
 }
