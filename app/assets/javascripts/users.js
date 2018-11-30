b5fa83260e3ee21ffcaa37852da16f531866e454
github username <github email address>
2018-11-29 20:15:52 -0500

    define association variable in User.loadBelongsToListener

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -32,7 +32,8 @@ $(function() {
 User.loadBelongsToListener = function() {
   $('div#user-designs').on('click', "a[class^='load-user']", function(e) {
     e.preventDefault()
-    var id = $(this).data('id') // stores the id of the user whose workout routines we want to view
+    var id = $(this).data('id') // stores the id of the user whose workout routines/training guides we want to view
+    var association = $(this).attr('class').split('-')[2] // "routines" or "guides"
     $(this).hide()
     $.get(`/users/${id}.json`)
     .done(User.loadClientWorkouts)
