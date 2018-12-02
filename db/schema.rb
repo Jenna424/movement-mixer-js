f8219934583cd38c7dabab6a96c9c5feefe141bf
github username <github email address>
2018-12-02 13:18:30 -0500

    create reviews table w/ user_id,routine_id,critique cols

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_181541) do
+ActiveRecord::Schema.define(version: 2018_12_02_181742) do
 
   create_table "equipment", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2018_12_02_181541) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "routine_id"
+    t.text "critique"
   end
 
   create_table "routine_targets", force: :cascade do |t|
