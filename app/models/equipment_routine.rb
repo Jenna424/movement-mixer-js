16bd66941da65a22837b8b96bb705d1745b0ecdb
github username <github email address>
2018-12-04 10:38:00 -0500

    add validation for weight numericality > 0 in EquipmentRoutine

--- app/models/equipment_routine.rb
+++ app/models/equipment_routine.rb
@@ -2,4 +2,5 @@ class EquipmentRoutine < ApplicationRecord
   belongs_to :equipment
   belongs_to :routine
   validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :weight, numericality: { greater_than: 0 }
 end
