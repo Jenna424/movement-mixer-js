9b1829635f9ef4cee12a78a48c99a1f542fa22df
github username <github email address>
2018-12-03 19:23:42 -0500

    define displaySuccessAlert(jsonObject) in shared.js

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -5,3 +5,19 @@ function errorHandler(jqXHR, textStatus, errorThrown) {
   }
   console.error(errorMessage)
 }
+
+function displaySuccessAlert(jsonObject) { // argument is JSON object representation of MovementRoutine instance or EquipmentRoutine instance
+  let alertMessage = 'You successfully added an exercise movement to this workout routine!'
+  if (jsonObject.constructor.name === 'EquipmentRoutine') {
+    alertMessage = 'You successfully added a piece of equipment to use in this workout!'
+  }
+  $('div#success-container').html(
+    `<div class="alert alert-success" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+      </button>
+      ${alertMessage}
+    </div>`
+  )
+  document.getElementById('success-container').scrollIntoView()
+}
