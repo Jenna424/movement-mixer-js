a90a22c7236b64da2628f9ab20307cea0a81b685
github username <github email address>
2018-12-04 10:50:43 -0500

    add allow_nil: true to weight validation in EquipmentRoutine

--- app/models/equipment_routine.rb
+++ app/models/equipment_routine.rb
@@ -2,5 +2,5 @@ class EquipmentRoutine < ApplicationRecord
   belongs_to :equipment
   belongs_to :routine
   validates :quantity, presence: true, numericality: { greater_than: 0 }
-  validates :weight, numericality: { greater_than: 0 }
+  validates :weight, numericality: { greater_than: 0 }, allow_nil: true
 end
