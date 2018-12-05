213a72946fed80a1e160360307d9708501e33eaf
github username <github email address>
2018-12-05 12:43:24 -0500

    change errorHandler function to handleError in shared.js

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -1,4 +1,4 @@
-function errorHandler(jqXHR, textStatus, errorThrown) {
+function handleError(jqXHR, textStatus, errorThrown) {
   var errorMessage = `The following error occurred: ${jqXHR.statusText} (status code ${jqXHR.status})`
   if (jqXHR.responseText.length) {
     errorMessage += `\nRead an in-depth description of the error below:\n${jqXHR.responseText}`
