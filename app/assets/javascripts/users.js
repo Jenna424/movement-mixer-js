cdfe734dff7d4569fac0f13b29d983b9a5c0f96d
github username <github email address>
2018-12-06 13:16:07 -0500

    omit double quotes in 'a[class^=load-user]'

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,10 +9,10 @@ function User(user) {
 $(() => {
   loadBelongsToDeclarer() // A workout routine belongs_to a client, and a training guide belongs_to a trainer
 })
-
+// loading the model that declares the belongs_to :user association (i.e. loading routines/guides that belong to the user)
 const loadBelongsToDeclarer = () => {
-  $('div#designer-data').on('click', "a[class^='load-user']", function(e) {
-    e.preventDefault()
+  $('div#designer-data').on('click', 'a[class^=load-user]', function(e) {
+    e.preventDefault() // prevent the default behavior of clicking the link, which would have been a normal HTTP GET request to "/users/:id", redirecting to the user show page and redrawing the DOM 
     var id = $(this).data('id') // stores the id of the user whose workout routines/training guides we want to view
     var loadAssociationFunction = User.loadUserRoutines
     if ($(this).attr('class').split('-')[2] === 'guides') {
