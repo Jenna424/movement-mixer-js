30f53d0d1f969e14b8623d5632e43f2e3871291c
github username <github email address>
2018-12-07 19:16:35 -0500

    change var to let; use const handleError arrow function syntax

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -5,8 +5,8 @@ const setLoggedOutLinks = () => {
   )
 }
 
-function handleError(jqXHR, textStatus, errorThrown) {
-  var errorMessage = `The following error occurred: ${jqXHR.statusText} (status code ${jqXHR.status})`
+const handleError = (jqXHR, textStatus, errorThrown) => {
+  let errorMessage = `The following error occurred: ${jqXHR.statusText} (status code ${jqXHR.status})`
   if (jqXHR.responseText.length) {
     errorMessage += `\nRead an in-depth description of the error below:\n${jqXHR.responseText}`
   }
