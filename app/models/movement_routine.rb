36ddc8776de9d0e74a0c1ea9b97706a55aa8e22a
github username <github email address>
2018-12-08 20:35:08 -0500

    delete presence: true validation for sets,reps

--- app/models/movement_routine.rb
+++ app/models/movement_routine.rb
@@ -2,6 +2,6 @@ class MovementRoutine < ApplicationRecord
   belongs_to :movement
   belongs_to :routine
   validates :technique, presence: true
-  validates :sets, presence: true, numericality: { greater_than: 0 }
-  validates :reps, presence: true, numericality: { greater_than: 0 }
+  validates :sets, numericality: { greater_than: 0 }
+  validates :reps, numericality: { greater_than: 0 }
 end
