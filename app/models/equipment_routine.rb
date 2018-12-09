6b3de68891bdc30e8094d2af508b2a5c6270d633
github username <github email address>
2018-12-08 20:38:56 -0500

    delete presence: true validation for quantity

--- app/models/equipment_routine.rb
+++ app/models/equipment_routine.rb
@@ -1,6 +1,6 @@
 class EquipmentRoutine < ApplicationRecord
   belongs_to :equipment
   belongs_to :routine
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than: 0 }
   validates :weight, numericality: { greater_than: 0 }, allow_nil: true
 end
