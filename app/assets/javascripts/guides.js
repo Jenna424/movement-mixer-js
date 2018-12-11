6211f1ab02917eec67b8dd50b8a73810c6f1fd25
github username <github email address>
2018-12-11 17:36:04 -0500

    indent <button> in Guide.createOrUpdate

--- app/assets/javascripts/guides.js
+++ app/assets/javascripts/guides.js
@@ -72,7 +72,7 @@ Guide.createOrUpdate = function(guideObject) {
   $('div#display-guide').html(guideHtml)
   $('div#message-container').html(
     `<div class="alert alert-success" role="alert">
-     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">×</span>
       </button>
       Your changes were successful!
