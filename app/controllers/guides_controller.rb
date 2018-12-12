145b097bf67831e85463673d78f41e5ef52f8de2
github username <github email address>
2018-12-12 10:34:00 -0500

    edit comments about guides#new and guides#create

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,5 +1,5 @@
 class GuidesController < ApplicationController
-  def new # GET '/movements/:movement_id/guides/new'=> 'guides#new'
+  def new # GET '/movements/:movement_id/guides/new'=> 'guides#new' (The form to create a new guide belonging to a movement is found in the movement show view AND in script#show-exercise-template in app/views/shared/_hs_templates.html.erb)
     @movement = Movement.find(params[:movement_id]) # finding the parent
     @guide = Guide.new # instance for nested resource form to wrap around
   end
@@ -7,7 +7,7 @@ class GuidesController < ApplicationController
   def create # POST '/movements/:movement_id/guides' => 'guides#create'
     movement = Movement.find(params[:movement_id]) # finding the parent movement instance
     guide = movement.guides.build(guide_params)
-    authorize guide
+    authorize guide # Only a trainer can create a training guide
     render json: guide, status: 201 if guide.save # If the training guide is NOT valid, the AJAX POST request will not fire at all
   end
 
