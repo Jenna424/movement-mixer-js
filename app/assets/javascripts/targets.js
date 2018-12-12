22c6c35631c472bb2fef35ab01297e24d444ad6c
github username <github email address>
2018-12-12 14:49:20 -0500

    delete Target.createListener in targets.js file

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -18,20 +18,7 @@ Target.prototype.formatLi = function() {
   return Target.targetTemplateFunction(this)
 }
 
-Target.createListener = function() {
-  $('#new_target').on('submit', function(e) {
-    e.preventDefault()
-    $targetAreaDiv = $('div#target-area-added')
-    var formData = $(this).serialize()
-    $.post('/targets', formData)
-    .done(Target.create)
-    .fail(function(jqXhrObject) { // handle a failure
-      var errorsArray = jqXhrObject.responseJSON.errors
-      var errorsString = errorsArray.join() // array elements are automatically comma-separated
-      alert(errorsString)
-    })
-  })
-}
+
 // json parameter below = JSON object representation of newly created AR target instance = response from AJAX POST request sent with $.post() method in Target.createListener()
 Target.create = function(json) {
   $("input[type='text']").val('') // empty the text field where trainer types in focus after form submission (so focus presence validation does not conflict with it)
