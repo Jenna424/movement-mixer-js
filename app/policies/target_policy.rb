98539a524a32c2859972f57f34230576fa8f163d
github username <github email address>
2018-12-12 12:07:04 -0500

    add create? in TargetPolicy class

--- app/policies/target_policy.rb
+++ app/policies/target_policy.rb
@@ -4,4 +4,8 @@ class TargetPolicy < ApplicationPolicy
       user.trainer? ? scope.all : scope.none # Only trainers can see the index of all workout target areas
     end
   end
+
+  def create?
+    user.trainer? # Only trainers can create a new target area in the form found on the Index of Workout Target Areas
+  end
 end
