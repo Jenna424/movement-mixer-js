1a78684c93dc2f38ad1822e9a54fe5d57cc61cbe
github username <github email address>
2018-12-12 12:10:53 -0500

    add destroy? to TargetPolicy class

--- app/policies/target_policy.rb
+++ app/policies/target_policy.rb
@@ -1,11 +1,15 @@
 class TargetPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      user.trainer? ? scope.all : scope.none # Only trainers can see the index of all workout target areas
+      user.trainer? ? scope.all : scope.none # Only trainers can see the Index of Workout Target Areas
     end
   end
 
   def create?
     user.trainer? # Only trainers can create a new target area in the form found on the Index of Workout Target Areas
+  end
+
+  def destroy?
+    user.trainer? # Any trainer can delete any target area in the Index of Workout Target Areas
   end
 end
