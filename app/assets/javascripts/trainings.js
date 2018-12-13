569399a9a6afff01f838f4671b9e0df82978440b
github username <github email address>
2018-12-13 17:04:00 -0500

    define Training.compileTrainingTemplate function

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -2,21 +2,25 @@ const Training = (training) => {
   this.id = training.id
   this.fitness_type = training.fitness_type
 }
+ 
+ $(() => {
+   Training.createListener()
+   Training.indexListener()
+   Training.destroyListener()
+ })
+ 
+ Training.createListener = function() {
+   $('form#new_training').on('submit', function(e) {
+     e.preventDefault()
+     let formData = $(this).serialize()
+     $('input[type=text]').val('') // empty the text_field where trainer typed in fitness_type
+     $.post('/trainings', formData)
+       .done(Training.create)
+       .fail(Training.testValidity)
+   })
+ }
 
-const bindEventListeners = () => {
-  Training.createListener()
-  Training.indexListener()
-  Training.destroyListener()
-}
-
-Training.createListener = function() {
-  $('form#new_training').on('submit', function(e) {
-  	e.preventDefault()
-  	$('ul#training-types-list').html('')
-  	let formData = $(this).serialize()
-  	$('input[type=text]').val('') // empty text_field where trainer enters value for fitness_type
-  	$.post('/trainings', formData)
-  	  .done(Training.create)
-  	  .fail(Training.testValidity)
-  })
-}
+ Training.compileTrainingTemplate = function() {
+   Training.trainingTemplateSource = $('#training-template').html()
+   Training.trainingTemplateFunction = Handlebars.compile(Training.trainingTemplateSource)
+ }
