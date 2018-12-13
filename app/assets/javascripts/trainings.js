e796888d9c7ae987e4dc189ae697506a72c0e66b
github username <github email address>
2018-12-13 17:12:22 -0500

    define Training.createListener in trainings.js

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -6,3 +6,15 @@ const Training = (training) => {
 const bindEventListeners = () => {
   Training.createListener()
 }
+
+Training.createListener = function() {
+  $('#new_training').on('submit', function(e) {
+  	e.preventDefault()
+  	$('ul#training-types-list').html('')
+  	let formData = $(this).serialize()
+  	$('input[type=text]').val('') // empty text_field where trainer typed in fitness_type
+  	$.post('/trainings', formData)
+  	  .done(Training.create)
+  	  .fail(Training.testValidity)
+  })
+}
