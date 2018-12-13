d8b747d070d6d45b3cd34beee83384bf5017ad21
github username <github email address>
2018-12-13 16:45:57 -0500

    define document ready w/ arrow function syntax in trainings.js

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -2,3 +2,9 @@ const Training = (training) => {
   this.id = training.id
   this.fitness_type = training.fitness_type
 }
+
+const bindEventListeners = () => {
+  Training.createListener()
+  Training.indexListener()
+  Training.destroyListener()
+}
