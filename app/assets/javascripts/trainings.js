5f73f7b12cc8e5b0e2f54864107af17dc95d9d02
github username <github email address>
2018-12-13 14:49:11 -0500

    indent .done() in Training.indexListener

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -80,7 +80,7 @@ Training.indexListener = function() {
   $('a.view-training-types').on('click', function(e) {
     e.preventDefault()
     $.get('/trainings')
-    .done(Training.indexTrainingTypes)
+      .done(Training.indexTrainingTypes)
   })
 }
 // trainingTypesArray parameter below is an array of JSON training objects = response from from AJAX GET request sent using $.get() in Training.indexListener()
