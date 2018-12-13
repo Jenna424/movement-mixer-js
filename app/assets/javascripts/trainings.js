aea8c985263c358fad20f201f4edc3ae56b07256
github username <github email address>
2018-12-13 18:00:37 -0500

    define Training.createListener

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -13,3 +13,14 @@ Training.compileTrainingTemplate = function() {
   Training.trainingTemplateSource = $('#training-template').html()
   Training.trainingTemplateFunction = Handlebars.compile(Training.trainingTemplateSource)
 }
+
+Training.createListener = function() {
+  $('form#new_training').on('submit', function(e) {
+    e.preventDefault()
+    let formData = $(this).serialize()
+    $('input[type=text]').val('') // empty the text_field where trainer types in fitness_type
+    $.post('/trainings', formData)
+      .done(Training.create)
+      .fail(Training.testValidity)
+  })
+}
