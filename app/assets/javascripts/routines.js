c9180d0e32392a06d3ac07fd5881ab234f8de214
github username <github email address>
2018-12-16 15:03:10 -0500

    don't make html of div.container div#message-container

--- app/assets/javascripts/routines.js
+++ app/assets/javascripts/routines.js
@@ -209,7 +209,6 @@ Routine.destroyListener = function() {
 // Below, routineResponse parameter = JSON object representation of AR routine instance that was just destroyed = response to AJAX DELETE request sent in Routine.destroyListener()
 Routine.destroy = function(routineResponse) {
   let newRoutine = new Routine(routineResponse)
-  $('div.container').html("<div id='message-container'></div>")
   history.pushState(null, null, '/')
   newRoutine.appendDeleteAlert()
 }
