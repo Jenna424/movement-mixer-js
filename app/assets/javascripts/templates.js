617033241d48b171938476d18f7a1a8f0ca4320a
github username <github email address>
2018-12-16 19:50:10 -0500

    change 11 variable declarations w/ var to const

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -4,17 +4,17 @@ $(function() {
 
 function checkIfTemplatesExist() {
   // The following variables store TRUE or FALSE (Self-Reminder: 0 is a falsy value in JS)
-  var routineTemplateExists = $('#routine-template').length
-  var techniqueTemplateExists = $('#technique-template').length
-  var listMovementTemplateExists = $('#list-movement-template').length
-  var guideTemplateExists = $('#guide-template').length
-  var showExerciseTemplateExists = $('#show-exercise-template').length
-  var mrTemplateExists = $('#mr-template').length
-  var editMovementRoutineTemplateExists = $('#edit-movement-routine-template').length
-  var editEquipmentRoutineTemplateExists = $('#edit-equipment-routine-template').length
-  var erTemplateExists = $('#er-template').length
-  var targetTemplateExists = $('#target-template').length
-  var trainingTemplateExists = $('#training-template').length
+  const routineTemplateExists = $('#routine-template').length
+  const techniqueTemplateExists = $('#technique-template').length
+  const listMovementTemplateExists = $('#list-movement-template').length
+  const guideTemplateExists = $('#guide-template').length
+  const showExerciseTemplateExists = $('#show-exercise-template').length
+  const mrTemplateExists = $('#mr-template').length
+  const editMovementRoutineTemplateExists = $('#edit-movement-routine-template').length
+  const editEquipmentRoutineTemplateExists = $('#edit-equipment-routine-template').length
+  const erTemplateExists = $('#er-template').length
+  const targetTemplateExists = $('#target-template').length
+  const trainingTemplateExists = $('#training-template').length
 
   // Conditionally compile Handlebars templates, depending on if the Handlebars template is present in the current DOM
   if (routineTemplateExists) { // User creates a new workout routine at "/routines/new", where these HS templates are rendered
