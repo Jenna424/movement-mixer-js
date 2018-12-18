8cf36673365b9b120e953023ef05b50e750b0fbf
github username <github email address>
2018-12-18 08:42:35 -0500

    change 'property' to 'attribute value' in editListener comment

--- app/assets/javascripts/equipment_routines.js
+++ app/assets/javascripts/equipment_routines.js
@@ -61,7 +61,7 @@ EquipmentRoutine.editListener = function() {
   $('a.edit-er').on('click', function(e) {
     e.preventDefault()
     let id = $(this).data('id') // stores the id of the instance of EquipmentRoutine join model being edited
-    $(this).hide() // once a.edit-er is clicked and I store its data-id property in the id variable, hide the link
+    $(this).hide() // once a.edit-er is clicked and I store its data-id attribute value in the id variable, hide the link
     $.get(`/ers/${id}/edit`)
       .done(EquipmentRoutine.displayEditForm)
       .fail(handleError)
