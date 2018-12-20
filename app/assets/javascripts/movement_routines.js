e862ea849ee47f74cb87947ee29e2cbb606c8640
github username <github email address>
2018-12-20 12:08:22 -0500

    delete mrDivsArray variable in addMovementToRoutine

--- app/assets/javascripts/movement_routines.js
+++ app/assets/javascripts/movement_routines.js
@@ -195,7 +195,6 @@ MovementRoutine.hideTechniqueListener = function() {
 // = successful JSON response I get back from the AJAX PATCH request sent in Routine.addExerciseToExistingWorkout()
 MovementRoutine.addMovementToRoutine = function(mrObject) {
   let newMr = new MovementRoutine(mrObject)
-  let mrDivsArray = $("div[id^='mr']")
   let filteredDivArray = mrDivsArray.filter(function() {
     return this.id === `mr-${newMr.id}-div` // this refers to each <div> element in the mrDivsArray
   })
