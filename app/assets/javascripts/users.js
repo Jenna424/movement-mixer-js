1eb6f3cb86334bf6212ac3dddc7ec96ebdd45981
github username <github email address>
2018-12-22 13:25:36 -0500

    call .on() on 'a[class^=load-user]' in loadBelongsToDeclarer

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,9 +12,9 @@ $(() => {
   User.indexListener()
   User.destroyListener()
 })
-// loading the associated model that declares the belongs_to :user association (i.e. loading routines/guides that belong to the user)
+// loading objects of the model that declares the belongs_to :user association (i.e. loading routines/guides that belong to the user)
 const loadBelongsToDeclarer = () => {
-  $('div#designer-data').on('click', 'a[class^=load-user]', function(e) {
+  $('a[class^=load-user]').on('click', function(e) {
     e.preventDefault() // prevent the default behavior of clicking the link, which would have been a normal HTTP GET request to "/users/:id", redirecting to the user show page and redrawing the DOM 
     let id = $(this).data('id') // stores the id of the user whose workout routines/training guides we want to view
     var loadAssociationFunction = User.loadUserRoutines // var-declared variables are function-scoped
@@ -29,9 +29,10 @@ const loadBelongsToDeclarer = () => {
 }
 
 User.loadUserRoutines = function(userObject) {
+  let newUser = new User(userObject)
   const $userRoutinesDiv = $('div#belongs-to-user') // variables declared with const are immutable, but the value of the variable CAN change, e.g., $userRoutinesDiv will always point to div#belongs-to-user, but the contents of the div change as routines are appended to it in the iteration
-  $userRoutinesDiv.html(`<h3>Workout Routines Designed by ${userObject.name}</h3>`)
-  userObject.routines.forEach(function(routineObject) {
+  $userRoutinesDiv.html(`<h3>Workout Routines Designed by ${newUser.name}</h3>`)
+  newUser.routines.forEach(function(routineObject) {
     $userRoutinesDiv.append(Routine.routineTemplateFunction(routineObject))
   })
   $userRoutinesDiv.addClass('client-routines')
