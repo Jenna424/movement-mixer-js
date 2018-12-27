ede90d224cc6a32ab1b84bfc0d0e96b68828a02e
github username <github email address>
2018-12-26 19:40:37 -0500

    edit comments in GuidesController

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,22 +1,22 @@
 class GuidesController < ApplicationController
-  def new # GET '/movements/:movement_id/guides/new'=> 'guides#new' (The form to create a new guide belonging to a movement is found in the movement show view AND in script#show-exercise-template in app/views/shared/_hs_templates.html.erb)
+  def new # get '/movements/:movement_id/guides/new' => 'guides#new' (The form to create a new guide belonging to a movement is found in the movement show view AND in script#show-movement-template in app/views/shared/_hs_templates.html.erb)
     @movement = Movement.find(params[:movement_id]) # finding the parent
     @guide = Guide.new # instance for nested resource form to wrap around
   end
 
-  def create # POST '/movements/:movement_id/guides' => 'guides#create'
+  def create # post '/movements/:movement_id/guides' => 'guides#create'
     movement = Movement.find(params[:movement_id]) # finding the parent movement instance
     guide = movement.guides.build(guide_params)
     authorize guide # Only a trainer can create a training guide
     render json: guide, status: 201 if guide.save # If the training guide is NOT valid, the AJAX POST request will not fire at all
   end
 
-  def index # GET '/movements/:movement_id/guides' => 'guides#index'
+  def index # get '/movements/:movement_id/guides' => 'guides#index'
     guides = Movement.find(params[:movement_id]).guides
     render json: guides, status: 200
   end
 
-  def edit # GET '/movements/:movement_id/guides/:id/edit' => 'guides#edit'
+  def edit # get '/movements/:movement_id/guides/:id/edit' => 'guides#edit'
     @movement = Movement.find(params[:movement_id])
     @guide = @movement.guides.find(params[:id])
     authorize @guide # Only the trainer who created the training guide can edit that guide in app/views/guides/edit.html.erb
