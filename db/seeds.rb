cb251537c3f5ed52cdc186043b82c623250ad181
github username <github email address>
2018-12-28 16:35:07 -0500

    delete users and comments from db/seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(
-  name: "Addison",
-  email: "addison@mm.com",
-  password: "adminaddison",
-  password_confirmation: "adminaddison",
-  role_requested: nil,
-  role: 3,
-  trainer_id: nil
-)
-
-User.create(
-  name: "Tracy",
-  email: "tracy@mm.com",
-  password: "trainertracy",
-  password_confirmation: "trainertracy",
-  role_requested: nil,
-  role: 2,
-  trainer_id: nil
-)
-
 targets = Target.create([
   { focus: 'Biceps' }, 
   { focus: 'Triceps' },
