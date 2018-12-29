c33a42391bfca2d7e4b9aca6ad5d16e2abc9c613
github username <github email address>
2018-12-29 15:18:07 -0500

    fix indentation in RoutinesHelper module

--- app/helpers/routines_helper.rb
+++ app/helpers/routines_helper.rb
@@ -1,13 +1,13 @@
 module RoutinesHelper
   def mr_instance(movement, routine)
-  	MovementRoutine.find_by(movement: movement, routine: routine)
+    MovementRoutine.find_by(movement: movement, routine: routine)
   end
 
   def er_instance(equipment, routine)
-  	EquipmentRoutine.find_by(equipment: equipment, routine: routine)
+    EquipmentRoutine.find_by(equipment: equipment, routine: routine)
   end
 
   def display_equipment_for(routine)
-  	routine.equipment.empty? ? "None" : routine.equipment.map{|e| e.name}.uniq.join(", ")
+    routine.equipment.empty? ? "None" : routine.equipment.map{|e| e.name}.uniq.join(", ")
   end
 end
